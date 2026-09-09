79186d9f837f7f196dba056f7c2b0493424be4f8
tahashieenavaz <tahashieenavaz@gmail.com>
2026-09-09 20:07:58 +0200

    refactor: moves the create canvas element logic to TypeScript initializer

--- src/classes/Board.ts
+++ src/classes/Board.ts
@@ -1,12 +1,11 @@
 export default class Board {
-  element: HTMLCanvasElement;
+  element: HTMLCanvasElement = document.createElement("canvas");
 
   constructor() {
     this.createCanvasElement();
   }
 
   createCanvasElement() {
-    this.element = document.createElement("canvas");
     this.element.width = innerWidth;
     this.element.height = innerWidth;
   }
