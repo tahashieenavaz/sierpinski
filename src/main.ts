243b84bc0b0d32df77f2f1f146641f59bfc76db3
tahashieenavaz <tahashieenavaz@gmail.com>
2026-09-10 00:00:30 +0200

    feat: update drawing coordinates in isolate callback for dynamic canvas size

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,8 @@ import Board from "@/classes/Board";
 const board = new Board();
 
 board.isolate((context: CanvasRenderingContext2D) => {
-  context.lineTo(10, 10);
   context.strokeStyle = "black";
+  context.lineTo(innerWidth / 2, innerHeight / 2);
+  context.lineWidth = 2;
   context.stroke();
 });
