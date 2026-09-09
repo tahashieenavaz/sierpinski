e1ab4776effc70aa9a00a60b8153bbd2f29d48d9
tahashieenavaz <tahashieenavaz@gmail.com>
2026-09-09 20:07:15 +0200

    refactor: moves creating canvas element logic to its separated method

--- src/classes/Board.ts
+++ src/classes/Board.ts
@@ -2,6 +2,10 @@ export default class Board {
   element: HTMLCanvasElement;
 
   constructor() {
+    this.createCanvasElement();
+  }
+
+  createCanvasElement() {
     this.element = document.createElement("canvas");
     this.element.width = innerWidth;
     this.element.height = innerWidth;
