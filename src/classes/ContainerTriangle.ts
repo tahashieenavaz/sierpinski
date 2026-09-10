00d000c9f86109f41b5b1954e90d11444b4d7371
tahashieenavaz <tahashieenavaz@gmail.com>
2026-09-11 00:32:20 +0200

    refactor: rewrites the triangle logic with Point class for more clarity

--- src/classes/ContainerTriangle.ts
+++ src/classes/ContainerTriangle.ts
@@ -2,21 +2,29 @@ import type Board from "./Board";
 import Point from "@/classes/Point";
 
 export default class ContainerTriangle {
+  A: Point;
+  B: Point;
+  C: Point;
+
+  constructor(factor: number = 5) {
+    const alpha = 1 / 2;
+    const beta = 1 / factor;
+    const gamma = 1 - 1 / factor;
+
+    this.A = new Point(alpha * window.innerWidth, beta * window.innerHeight);
+    this.B = new Point(beta * window.innerWidth, gamma * window.innerHeight);
+    this.C = new Point(gamma * window.innerWidth, gamma * window.innerHeight);
+  }
+
   draw(board: Board) {
     board.isolate((context) => {
       context.strokeStyle = "black";
       context.lineWidth = 1;
 
-      const factor = 5;
-      const alpha = 1 / 2;
-      const beta = 1 / factor;
-      const gamma = 1 - 1 / factor;
-
-      context.moveTo(alpha * innerWidth, beta * innerHeight);
-      context.lineTo(beta * innerWidth, gamma * innerHeight);
-      context.lineTo(gamma * innerWidth, gamma * innerHeight);
-      context.lineTo(gamma * innerWidth, gamma * innerHeight);
-      context.lineTo(alpha * innerWidth, beta * innerHeight);
+      context.moveTo(this.A.x, this.A.y);
+      context.lineTo(this.B.x, this.B.y);
+      context.lineTo(this.C.x, this.C.y);
+      context.lineTo(this.A.x, this.A.y);
       context.stroke();
     });
   }
