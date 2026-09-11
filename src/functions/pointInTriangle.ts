b131bfeecad5eb2e521705c60b004af7974f07c5
tahashieenavaz <tahashieenavaz@gmail.com>
2026-09-12 00:15:49 +0200

    feat: implement pointInTriangle function to generate a point within a triangle

--- src/functions/pointInTriangle.ts
+++ src/functions/pointInTriangle.ts
@@ -1,4 +1,6 @@
 import Point from "@/classes/Point";
+import randomNumber from "./randomNumber";
+import isInsideTriangle from "./isInsideTriangle";
 
 export default function pointInTriangle(
   A: Point,
@@ -6,4 +8,13 @@ export default function pointInTriangle(
   C: Point,
   x_range: number = window.innerWidth,
   y_range: number = window.innerHeight,
-) {}
+): Point {
+  let x = randomNumber(0, x_range);
+  let y = randomNumber(0, y_range);
+
+  while (isInsideTriangle(new Point(x, y), A, B, C)) {
+    x = randomNumber(0, x_range);
+    y = randomNumber(0, y_range);
+  }
+  return new Point(x, y);
+}
