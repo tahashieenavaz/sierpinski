44e2b3d5ec5ac9dbdb6a7993dcd961e8f3fa53ca
tahashieenavaz <tahashieenavaz@gmail.com>
2026-09-12 00:12:17 +0200

    feat: add error handling for invalid range in randomNumber function

--- src/functions/randomNumber.ts
+++ src/functions/randomNumber.ts
@@ -1,4 +1,10 @@
 export default function randomNumber(low: number, high: number) {
+  if (low > high) {
+    throw new Error(
+      `randomNumber received a low(${low}) that is bigger than high(${high}). `,
+    );
+  }
+
   const difference = high - low;
   return Math.floor(Math.random() * difference + low);
 }
