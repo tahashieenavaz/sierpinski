2f540f2e7f4b5201130107e98f5ce650399422a1
tahashieenavaz <tahashieenavaz@gmail.com>
2026-09-12 12:43:49 +0200

    fix: correct random index calculation in oneOf function and update main.ts to use it

--- src/functions/oneOf.ts
+++ src/functions/oneOf.ts
@@ -3,6 +3,6 @@ import randomNumber from "./randomNumber";
 export default function oneOf<OneOfCollectionItem>(
   collection: OneOfCollectionItem[],
 ): OneOfCollectionItem {
-  const randomIndex = randomNumber(0, collection.length);
+  const randomIndex = randomNumber(0, collection.length - 1);
   return collection[randomIndex];
 }
--- src/main.ts
+++ src/main.ts
@@ -2,18 +2,29 @@ import "@stylesheets/style.css";
 import Board from "@/classes/Board";
 import ContainerTriangle from "@/classes/ContainerTriangle";
 import pointInTriangle from "@/functions/pointInTriangle";
+import oneOf from "@/functions/oneOf";
 
 const board = new Board();
 const containerTriangle = new ContainerTriangle();
 containerTriangle.draw(board);
-const point = pointInTriangle(
+
+const randomPoint = pointInTriangle(
   containerTriangle.A,
   containerTriangle.B,
   containerTriangle.C,
 );
-
+const randomCorner = oneOf([
+  containerTriangle.A,
+  containerTriangle.B,
+  containerTriangle.C,
+]);
 board.isolate((context) => {
-  context.arc(point.x, point.y, 1, 0, Math.PI * 2);
+  context.arc(randomPoint.x, randomPoint.y, 1, 0, Math.PI * 2);
   context.fillStyle = "black";
   context.fill();
+});
+board.isolate((context) => {
+  context.moveTo(randomPoint.x, randomPoint.y);
+  context.lineTo(randomCorner.x, randomCorner.y);
+  context.stroke();
 });
